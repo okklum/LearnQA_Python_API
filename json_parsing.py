64355f7f3cbe1a50fc60ec6c029d9fb61ce3b53b
Alisa Lapteva <okklum@gmail.com>
2022-02-17 00:07:14 +0300

    ДЗ 5. Убрала проверку ключей

--- json_parsing.py
+++ json_parsing.py
@@ -4,12 +4,4 @@ strData = '{"messages":[{"message":"This is the first message","timestamp":"2021
           'this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 dictData = json.loads(strData)
 key = "messages"
-
-if key in dictData:
-    key1 = "message"
-    if key1 in dictData:
-        print(dictData["messages"][1]["message"])
-    else:
-        print(f"Key {key1} not found in modified JSON")
-else:
-    print(f"Key {key} not found in JSON")
+print(dictData["messages"][1]["message"])
